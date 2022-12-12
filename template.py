98c08bd2c49b2afe12a693b532637b7128c7955b
Andrew Truong <itsandrewtruong@gmail.com>
2022-12-12 13:48:48 -0500

    test

--- template.py
+++ template.py
@@ -5,18 +5,25 @@ report = wr.Report(
     title="Hello from Github Actions!",
     description="This report was generated from a github action on the marketplace",
     blocks=[
-        wr.H1([
+        wr.P([
             'To learn more, check out the action ',
-            wr.Link("on the github marketplace", "https://github.com/marketplace/actions/generate-weights-biases-report")
+            wr.Link(
+                "on the github marketplace",
+                link="https://github.com/marketplace/actions/generate-weights-biases-report"
+            )
         ]),
+        wr.H1("Here are some charts"),
+        wr.P("We wrote everything declaratively!"),
         wr.PanelGrid(
-            runsets=[wr.Runset(project="report-api-quickstart")],
+            runsets=[wr.Runset(project='lineage-example'), wr.Runset(project="report-api-quickstart")],
             panels=[
                 wr.LinePlot(y='val_acc', title='Validation Accuracy over Time'),
                 wr.BarPlot(metrics='acc'),
                 wr.MediaBrowser(media_keys='img', num_columns=1)
             ],
         ),
+        wr.H1("Here is some artifact lineage"),
+        wr.WeaveBlockArtifact("megatruong", "lineage-example", artifact="model-1", tab="lineage")
     ]
 ).save()
 
